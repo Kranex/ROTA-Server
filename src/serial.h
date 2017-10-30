83af40a9a6ba4f0cf3092349cc7b676b5bdae210
Kranex <oliverstrik@gmail.com>
2017-10-30 18:40:52 +0100

    fix

--- src/serial.h
+++ src/serial.h
@@ -1,6 +1,6 @@
 #ifndef SERIAL_H
 #define SERIAL_H
-int serialport_init(const char* serialport);
+void serialport_init(const char* serialport);
 int serialport_write(const signed char* str, int len);
 void serial_close();
 
