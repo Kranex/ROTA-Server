5aba3f258f1f1bc4b19cf70952bde856c7f585f0
Kranex <oliverstrik@gmail.com>
2017-10-30 19:15:34 +0100

    A B
    updated serial.c

--- src/serial.c
+++ src/serial.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[]) {
   char buf[20], dat[20], use[1];
   int rc,n;
   //baudrate = 9600;
-  fd = serialport_init("/dev/ttyACM0", baudrate);
+  fd = serialport_init("/dev/ttyACM0", baudrate);   
   if(fd==-1) return -1;
   usleep(3000 * 1000 );
   while(1) {
@@ -112,47 +112,18 @@ int main(int argc, char *argv[]) {
   exit(EXIT_SUCCESS);
 } // end main
 
-int serialport_writebyte( int fd, uint8_t b) {
-  int n = write(fd,&b,1);
-  if( n!=1)
-  return -1;
-  return 0;
-}
-
-
-
-int serialport_write(int fd, const char* str) {
+int serialport_write(int fd, const signed char* str) {
   int len = strlen(str);
   int n = write(fd, str, len);
-  if( n!=len )
-  return -1;
-  return n;
+  return ( n!=len)  ?  (-1) : (n);
 }
-
-int serialport_read_until(int fd, char* buf, char until) {
-  char b[1];
-  int i=0;
-  do {
-    int n = read(fd, b, 1);  // read a char at a time
-    if( n==-1) return -1;    // couldn't
-    if( n==0 ) {
-      usleep( 10 * 1000 ); // wait 10 msec try again
-      continue;
-    }
-    buf[i] = b[0]; i++;
-  } while( b[0] != until );
-  buf[i] = 0;  // null terminate the string
-  return i;
-}
-
-
 
 // takes the string name of the serial port (e.g. "/dev/tty.usbserial","COM1")
 // and a baud rate (bps) and connects to that port at that speed and 8N1.
 // opens the port in fully raw mode so you can send binary data.
 // returns valid fd, or -1 on error
 
-int serialport_init(const char* serialport, int baud) {
+int serialport_init(const char* serialport) {
   struct termios toptions;
   int fd;
   //fprintf(stderr,"init_serialport: opening port %s @ %d bps\n",
@@ -171,30 +142,8 @@ int serialport_init(const char* serialport, int baud) {
     return -1;
   }
 
-  speed_t brate = baud; // let you override switch below if needed
-
-  switch(baud) {
-    case 4800:   brate=B4800;   break;
-    case 9600:   brate=B9600;   break;
-    // if you want these speeds, uncomment these and set #defines if Linux
-    //#ifndef OSNAME_LINUX
-    //    case 14400:  brate=B14400;  break;
-    //#endif
-    case 19200:  brate=B19200;  break;
-    //#ifndef OSNAME_LINUX
-    //    case 28800:  brate=B28800;  break;
-    //#endif
-    //case 28800:  brate=B28800;  break;
-    case 38400:  brate=B38400;  break;
-    case 57600:  brate=B57600;  break;
-    case 115200: brate=B115200; break;
-
-  }
-
-  cfsetispeed(&toptions, brate);
-  cfsetospeed(&toptions, brate);
-
-
+  cfsetispeed(&toptions, B9600);
+  cfsetospeed(&toptions, B9600);
 
   // 8N1
 
