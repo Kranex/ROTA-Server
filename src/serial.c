d8c2fafc1e391b2d269119c1bbda34f405d037f1
Kranex <oliverstrik@gmail.com>
2017-10-31 22:45:13 +0100

    update baud

--- src/serial.c
+++ src/serial.c
@@ -81,8 +81,8 @@ int serialport_init(const char* serialport) {
     return -1;
   }
 
-  cfsetispeed(&toptions, B9600);
-  cfsetospeed(&toptions, B9600);
+  cfsetispeed(&toptions, B115200);
+  cfsetospeed(&toptions, B115200);
 
   // 8N1
 
